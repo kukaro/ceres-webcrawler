e35fa4371e7bef171ec1ef566069aa5343dddcdd
kukaro <justkukaro@naver.com>
2019-03-16 23:44:42 +0900

    crawl

--- src/01.py
+++ src/01.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import platform
 
 driver = None
 os_type = platform.platform()
 
-if os_type.find('Darwin')==0:
+if os_type.find('Darwin') == 0:
     driver = webdriver.Chrome('../chromedriver_mac64')
 else:
     driver = webdriver.Chrome('../chromedriver_linux64')
-# driver = webdriver.PhantomJS('../phantomjs-2.1.1-macosx/bin/phantomjs')
 driver.implicitly_wait(3)
 
-driver.get('https://nid.naver.com/nidlogin.login')
-driver.implicitly_wait(3)
-driver.find_element_by_name('id').send_keys('naver_id')
-driver.find_element_by_name('pw').send_keys('mypassword1234')
-driver.implicitly_wait(3)
-driver.close()
+find_url = 'https://search.naver.com/search.naver?where=article&query={{}}&ie=utf8&st=rel&date_option=2&date_from=&date_to=&board=&srchby=text&dup_remove=1&cafe_url=&without_cafe_url=&sm=tab_opt&nso=so%3Ar%2Cp%3A1w%2Ca%3Aall&t=0&mson=0&prdtype=0'
+find_keyworld = '파이어 레드'
+
+
+def find(url, keworld):
+    url = url.replace('{{}}', keworld)
+    driver.get(url)
+    html = BeautifulSoup(driver.page_source, 'html.parser')
+    items = html.select('ul#elThumbnailResultArea > li > dl > dd.sh_cafe_passage ')
+    for key, value in enumerate(items):
+        print("%i: %s" % (key, value.text))
+    driver.close()
+
+
+find(find_url, find_keyworld)
